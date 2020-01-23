f4fa7e248d4ab4e957c42cb369f55f7e66bee18a
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-22 23:29:56 -0500

    asd

--- src/sub-components/WhatIsEbuIndicator.js
+++ src/sub-components/WhatIsEbuIndicator.js
@@ -8,12 +8,13 @@ export default function WhatIsEbuIndicator(){
     <div className="card">
       <div className="card-body">
         <b>EBU Indicator</b> is a script that finds most powerful patterns on Finance Charts like <b>Forex</b>, <b>Stock Markets</b>, <b>Warrants</b> etc.<br /><br /> We wrote the script with <b><a href="https://www.tradingview.com/blog/en/introducing-pine-script-4-12626/" target="_blank">Pine Script v4</a></b> and it's just usable on <b><a href="https://www.tradingview.com/" target="_blank">TradingView Platform.</a></b><br /><br />
-        In script we are using the <b><a href="https://www.investopedia.com/ask/answers/030415/what-zig-zag-indicator-formula-and-how-it-calculated.asp" target="_blank">ZigZag</a></b> and <b><a href="https://www.investopedia.com/ask/answers/05/fibonacciretracement.asp" target="_blank">Fibonacci</a></b> levels for recognizing patterns. The script checks all pivot highs and pivot lows with Fibonacci numbers. If the functions that is located on script are true, script draws the pattern lines automatically.<br />
+        In script we are using the <b><a href="https://www.investopedia.com/ask/answers/030415/what-zig-zag-indicator-formula-and-how-it-calculated.asp" target="_blank">ZigZag</a></b> and <b><a href="https://www.investopedia.com/ask/answers/05/fibonacciretracement.asp" target="_blank">Fibonacci</a></b> levels for recognizing patterns. The script checks all pivot highs and pivot lows with Fibonacci numbers. If the functions that is located on script are true, script draws the pattern lines automatically.<br /><br />
+      <div className="text-muted"><b><u>Note: EBU Indicator is recommended for people who are not totally alien to subject. But still, everyone can learn!</u></b></div>
       <br />
         The techniques(patterns) we are using are not a secret. Only secret is finding those <b>automatically.</b> Before this script we were looking and finding patterns manually and we can tell it's not a thing you want to do constantly. This is basically the main reason that why we wrote the script.
       <br /><br />
       <h5><b>EBU Indicator is <u>a script...</u></b></h5>
-      ...that will <b>help you on deciding</b> on buying or selling <b>stocks</b>, <b>positions, commodities etc.</b>
+      ...that will <b>help you on deciding</b> about buying or selling <b>stocks</b>, <b>positions, commodities etc.</b>
       <div style={{marginTop: 5}}>...that you can expect <b>0% - 300% profit rates.</b></div>
       <div style={{marginTop: 5}}>...that you can put on work at any <b>Financial Chart.</b></div>
       <div style={{marginTop: 5}}>...that works <b>only</b> on <b>TradingView Platform.</b></div>
