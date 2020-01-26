4b84f2bc19b1688452ff39649ea6676d4d57b178
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-26 16:40:52 -0500

    asf

--- src/App.js
+++ src/App.js
@@ -13,37 +13,53 @@ import BecomeAnAffiliate from './main-components/BecomeAnAffiliate.js';
 import FAQ from './main-components/FAQ.js';
 import HowTo from './main-components/HowTo.js';
 import Screenshots from './main-components/Screenshots.js';
+import { Loader, Segment, Dimmer } from 'semantic-ui-react';
 import $ from 'jquery';
 import { Image } from 'react-bootstrap';
 
 class App extends React.Component {
-  // componentDidMount(){
-  //   $('#pricing-button').click(function(){
-  //       $('html, body').animate({
-  //           scrollTop: $( $(this).attr('href') ).offset().top
-  //       }, 500);
-  //       return false;
-  //   });
-  // }
+  constructor(){
+    super();
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount(){
+    this.setState({loading: false})
+  }
+
+
 
   render(){
-    return (
-      <>
-      <NavbarView />
-       <Switch>
-         <Route exact path='/' component={IndexPage} key='index' />
-         <Route exact path='/about-us' component={AboutUs} key='about-us' />
-         <Route exact path='/reach-us' component={ContactForm} key='contact-us' />
-         <Route exact path='/pricing' component={Pricing} key='contact-us' />
-         <Route exact path='/how-to' component={HowTo} key='how-to' />
-         <Route exact path='/faq' component={FAQ} key='FAQ' />
-         <Route exact path='/screenshots' component={Screenshots} key='FAQ' />
-         <Route exact path='/become-an-affiliate' component={BecomeAnAffiliate} key='become-an-affiliate' />
-         <Route path='/payment-successful/:subId' component={PaymentSuccessfulPage} key='index' />
-      </Switch>
-       <ScrollUpButton style={{marginBottom: 30}}/>
-       </>
-    );
+    console.log(this.state.loading)
+    if(this.state.loading){
+      return(
+        <Segment>
+          <Dimmer active>
+            <Loader />
+          </Dimmer>
+        </Segment>
+      )
+    } else {
+      return (
+        <>
+        <NavbarView />
+         <Switch>
+           <Route exact path='/' component={IndexPage} key='index' />
+           <Route exact path='/about-us' component={AboutUs} key='about-us' />
+           <Route exact path='/reach-us' component={ContactForm} key='contact-us' />
+           <Route exact path='/pricing' component={Pricing} key='contact-us' />
+           <Route exact path='/how-to' component={HowTo} key='how-to' />
+           <Route exact path='/faq' component={FAQ} key='FAQ' />
+           <Route exact path='/screenshots' component={Screenshots} key='FAQ' />
+           <Route exact path='/become-an-affiliate' component={BecomeAnAffiliate} key='become-an-affiliate' />
+           <Route path='/payment-successful/:subId' component={PaymentSuccessfulPage} key='index' />
+        </Switch>
+         <ScrollUpButton style={{marginBottom: 30}}/>
+         </>
+      );
+    }
   }
 }
 
