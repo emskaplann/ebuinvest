94fa3862af19d5dc67af86bd3a80b2158f66de4b
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-26 15:58:56 -0500

    asf

--- src/main-components/HowTo.js
+++ src/main-components/HowTo.js
@@ -20,9 +20,27 @@ export default class HowTo extends React.Component {
         <div className="card">
           <div className="card-body">
             <h3><b>How to?</b></h3>
+
             <h4><b>1. How to setup our Indicator?</b></h4>
-            We have a guide for just that. Please click <a href="https://medium.com/@snmzbrby/ebu-indicator-guide-b9bfe60916a3">here.</a> Also we publish weekly stories about finance on that account.
+            We have a guide for just that. Please click <a href="https://medium.com/@snmzbrby/ebu-indicator-guide-b9bfe60916a3"><b>here.</b></a> Also we publish weekly stories about finance on that account.
             This guide covers not only setup, It also includes how to take actions(buy or sell) based on our Indicator.
+            <br /><br />
+
+            <h4><b>2. How to take actions(buy or sell) based on our Indicator?</b></h4>
+            Our setup and actions guide is in the same guide. Please click <a href="https://medium.com/@snmzbrby/ebu-indicator-guide-b9bfe60916a3"><b>here</b></a> to see the guide.
+            <br /><br />
+
+            <h4><b>3. How to get motivated?</b></h4>
+            Right now we are creating a playlist for motivation. The link is here but it's not big right now :).<br />
+            <span className="text-muted"><b>Note:</b> We don't create or make these videos, it's just sources we found helpful.</span>
+            <br /><br />
+
+            <h4><b>4. How to learn basics about finance, stock markets, forex?</b></h4>
+            Right now we are creating a playlist for education series. We still don't have any good source to share right now but we'll update here ASAP.<br />
+            <span className="text-muted"><b>Note:</b> We don't create or make these videos, it's just sources we found helpful.</span>
+            <br /><br />
+
+
           </div>
         </div>
         <br />
