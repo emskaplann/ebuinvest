19d6e5080d8f41128fd5052fb5038c685f40d89a
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-26 15:48:33 -0500

    asfg

--- src/sub-components/ContactForm.js
+++ src/sub-components/ContactForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactContactForm from 'react-mail-form';
 import { Container, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import JoinUs from '../sub-components/JoinUs.js';
 
 export default class ContactForm extends React.Component{
   render(){
@@ -12,7 +13,7 @@ export default class ContactForm extends React.Component{
             <h3><b>How to Contact Us?</b></h3>
             You can simply press<a target="_blank" href="mailto:contact@ebuinvest.com?subject=Support - Your Subject" style={{textDecoration: 'none'}}> <b>here</b></a> to compose a mail to us.
             Or you can compose yourself, to this mail address <b>contact@ebuinvest.com.</b><br/>
-          <div style={{marginTop: 10}}><a href="https://t.me/joinchat/NeUKX1EKpn9Bk03V2JldlA" target="_blank">Join our Free Telegram Group for daily insights and to get your questions answered in light speed.</a></div>
+          <div style={{marginTop: 10}}><a href="https://t.me/joinchat/NeUKX1EKpn9Bk03V2JldlA" target="_blank"><b>Join our Free Telegram Group for daily insights and to get your questions answered in light speed.</b></a></div>
           <div style={{marginTop: 10}}>We don't present a contact form on our website because of the spammers 🤖</div><br /><br />
           <span onClick={this.props.history.goBack}><b>{"< Go back "}</b></span>| <Link to="/" style={{textDecoration: 'none', color: "black"}}><b>{"< Go to Main Page"}</b></Link>
         </div>
@@ -31,6 +32,7 @@ export default class ContactForm extends React.Component{
             </div>
           </form>
           </div>
+          <JoinUs />
           <br />
           <Image src="/tgroar.jpeg" fluid={true} style={{borderRadius: 5}} />
           <br /><br />
