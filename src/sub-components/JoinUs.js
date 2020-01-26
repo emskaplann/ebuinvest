975cfdf2a9e3366ae5a7618f26a773ad92cf292d
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-26 15:39:09 -0500

    asf

--- src/sub-components/JoinUs.js
+++ src/sub-components/JoinUs.js
@@ -9,19 +9,24 @@ export default function JoinUs(){
       <div className="card">
         <div className="card-body" style={{textAlign: 'center'}}>
             <Row>
-              <Col xs={4} sm={4} md={4} lg={4}>
+              <Col xs={3} sm={3} md={3} lg={3}>
                 <a href="https://t.me/joinchat/NeUKX1EKpn9Bk03V2JldlA" target="_blank" className="footer-links">
                   <i className="fab fa-telegram" style={{fontSize: '5rem'}} aria-hidden="true"></i>
                 </a>
               </Col>
-              <Col xs={4} sm={4} md={4} lg={4}>
+              <Col xs={3} sm={3} md={3} lg={3}>
                 <a href="https://www.instagram.com/ebuinvest/" target="_blank" className="ml-2 footer-links">
                   <i className="fab fa-instagram" style={{fontSize: '5rem'}} aria-hidden="true"></i>
                 </a>
               </Col>
-              <Col xs={4} sm={4} md={4} lg={4}>
+              <Col xs={3} sm={3} md={3} lg={3}>
                 <a href="https://www.twitter.com/ebuinvest/" target="_blank" className="ml-2 footer-links">
                   <i className="fab fa-twitter" style={{fontSize: '5rem'}} aria-hidden="true"></i>
+                </a>
+              </Col>
+              <Col xs={3} sm={3} md={3} lg={3}>
+                <a href="https://medium.com/@snmzbrby" target="_blank" className="ml-2 footer-links">
+                  <i className="fab fa-medium" style={{fontSize: '5rem'}} aria-hidden="true"></i>
                 </a>
               </Col>
             </Row>
