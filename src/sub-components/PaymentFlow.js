0fe8c73aee9483252415738034f20145014eb8a6
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-26 14:51:09 -0500

    asf

--- src/sub-components/PaymentFlow.js
+++ src/sub-components/PaymentFlow.js
@@ -15,11 +15,11 @@ export default function WhatIsEbuIndicator(){
         <b>How is The Flow?</b>
         <div style={{marginTop: 5}}><b>1 -</b> After you click on button and complete the PayPal process, we'll send you an automated e-mail that contains a form link.</div>
         <span className="text-muted"><b>Note: <u>You don't pay anything until Free Trial ends.</u> <u>And you cancel any time you want.</u></b> We just need you to subscribe with PayPal because we need to track free trials. Therefore, PayPal doesn't share any Payment Information with Merchants.</span>
-        <div style={{marginTop: 5}}><b>2 -</b> In this step, <u>if you don't have a TradingView Account, you need to open an account to fill the form.</u> You can open the account from <a href="https://www.tradingview.com/" target="_blank">here.</a></div>
-        <div style={{marginTop: 5}}><b>3 -</b> You need fill the form that we send you with the e-mail. <b>And keep in mind you should put the e-mail address that you have PayPal account on.</b></div> If you didn't get the mail, please contact us from <Link to="/reach-us">here.</Link>
-        <div style={{marginTop: 5}}><b>4 -</b> In the form we ask for basic information like Telegram username, TradingView username, etc... We ask Telegram for <b>Telegram Group</b>, where you can ask anything and where you meet your <b>new community.</b></div>
-        <div style={{marginTop: 5}}><b>5 -</b> With the link you get you can follow our <b>guide</b> to setup and test our indicator. And there is <b>no limitations</b> on Free Trial.</div>
-        <div style={{marginTop: 5}}><b>6 -</b> If you still have question(s) please don't hesitate to contact us from <b>contact@ebuinvest.com</b>. We will try our best to reply ASAP.</div>
+        <div style={{marginTop: 12}}><b>2 -</b> In this step, <u>if you don't have a TradingView Account, you need to open an account to fill the form.</u> You can open the account from <a href="https://www.tradingview.com/" target="_blank">here.</a></div>
+        <div style={{marginTop: 12}}><b>3 -</b> You need fill the form that we send you with the e-mail. <b>And keep in mind you should put the e-mail address that you have PayPal account on.</b> If you didn't get the mail, please contact us from <Link to="/reach-us">here.</Link></div>
+        <div style={{marginTop: 12}}><b>4 -</b> In the form we ask for basic information like Telegram username, TradingView username, etc... We ask Telegram for <b>Telegram Group</b>, where you can ask anything and where you meet your <b>new community.</b></div>
+        <div style={{marginTop: 12}}><b>5 -</b> With the link you get you can follow our <b>guide</b> to setup and test our indicator. And there is <b>no limitations</b> on Free Trial.</div>
+        <div style={{marginTop: 12}}><b>6 -</b> If you still have question(s) please don't hesitate to contact us from <b>contact@ebuinvest.com</b>. We will try our best to reply ASAP.</div>
       </div>
     </div>
     </>
