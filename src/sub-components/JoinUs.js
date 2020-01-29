39b40db710d9790e9703f61b6da6b1e0237ad70f
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-28 21:22:44 -0500

    asf

--- src/sub-components/JoinUs.js
+++ src/sub-components/JoinUs.js
@@ -8,27 +8,19 @@ export default function JoinUs(){
       <Divider horizontal style={{color: "white"}}>JOIN US</Divider>
       <div className="card">
         <div className="card-body" style={{textAlign: 'center'}}>
-            <Row>
-              <Col xs={3} sm={3} md={3} lg={3}>
-                <a href="https://t.me/joinchat/NeUKX1EKpn9Bk03V2JldlA" target="_blank" rel="noopener noreferrer" className="footer-links">
+            <Row style={{justifyContent: 'space-around'}}>
+                <a href="https://t.me/joinchat/NeUKX1EKpn9Bk03V2JldlA" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-telegram" style={{fontSize: '5rem'}} aria-hidden="true"></i>
                 </a>
-              </Col>
-              <Col xs={3} sm={3} md={3} lg={3}>
-                <a href="https://www.instagram.com/ebuinvest/" target="_blank" rel="noopener noreferrer" className="ml-2 footer-links">
+                <a href="https://www.instagram.com/ebuinvest/" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-instagram" style={{fontSize: '5rem'}} aria-hidden="true"></i>
                 </a>
-              </Col>
-              <Col xs={3} sm={3} md={3} lg={3}>
-                <a href="https://www.twitter.com/ebuinvest/" target="_blank" rel="noopener noreferrer" className="ml-2 footer-links">
+                <a href="https://www.twitter.com/ebuinvest/" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-twitter" style={{fontSize: '5rem'}} aria-hidden="true"></i>
                 </a>
-              </Col>
-              <Col xs={3} sm={3} md={3} lg={3}>
-                <a href="https://medium.com/@snmzbrby" target="_blank" rel="noopener noreferrer" className="ml-2 footer-links">
+                <a href="https://medium.com/@snmzbrby" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-medium" style={{fontSize: '5rem'}} aria-hidden="true"></i>
                 </a>
-              </Col>
             </Row>
         </div>
       </div>
