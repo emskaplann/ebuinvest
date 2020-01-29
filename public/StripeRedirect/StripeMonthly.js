4a8a814c651a1277ee84625115664cadc4c9e335
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-28 20:41:16 -0500

    asf

--- public/StripeRedirect/StripeMonthly.js
+++ public/StripeRedirect/StripeMonthly.js
@@ -1,9 +1,9 @@
-var stripe = Stripe('pk_test_YJZiIQadCitjxkefrqHysj0g00BNRtnusD');
+var stripe = Stripe('pk_live_Y4ZpMfxp2w8fDkilkdQJsJvT00s1VJ4euh');
 
 stripe.redirectToCheckout({
   items: [{
     // Replace with the ID of your plan
-    plan: 'plan_GdL9mX1ZYK1j0r',
+    plan: 'plan_GdKf2X03IdtsrH',
     quantity: 1,
   }],
   successUrl: 'https://ebuinvest.com/',
